76b415d1780cbb6f790dbd297cae02fb45231444
diankuijpens <37109116+diankuijpens@users.noreply.github.com>
2018-07-14 04:13:11 +0200

    Add files via upload

---
+++ javascript testen 1.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Testen van Javascript</title>
+</head>
+<body>
+<script type="text/javascript">
+	document.write("hallo mensen");
+	console.log("hallo people");
+</script>
+</body>
+</html>
+
+<script src="javascript"></script>
---
+++ javascript.js
@@ -0,0 +1,5 @@
+<script>
+var netto = parsefloat (window.prompt("400"));
+var btw = 400 *0.20;
+var bruto = 400 + 0.20;
+</script>
